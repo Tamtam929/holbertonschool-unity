2672f00988da8a224f4329b27ce0a866aceb1480
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-07-18 12:39:07 -0500

    unity ui

---
+++ unity-assets_ui/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    void OnTriggerExit(Collider other)
+    {
+        Timer otherTimer = other.GetComponent<Timer>();
+        if (otherTimer != null)
+            otherTimer.enabled = true;
+    }
+}
