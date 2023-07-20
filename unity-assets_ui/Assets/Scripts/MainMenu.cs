ecfca21744a47b6d4ec6d1dcb8566d8226b945fd
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-07-19 21:14:34 -0500

    1

--- unity-assets_ui/Assets/Scripts/MainMenu.cs
+++ unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LevelSelect(int level)
     {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Options()
+    {
+        SceneManager.LoadScene("Options");
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+        Debug.Log("Exit Game");
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit();
+    #endif
     }
+    
 }
