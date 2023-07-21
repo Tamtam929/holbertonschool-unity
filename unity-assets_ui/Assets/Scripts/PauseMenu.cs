b630cdd09de1428d8bde56d8dbc92e547d166094
Tammy Junk <112114716+Tamtam929@users.noreply.github.com>
2023-07-20 19:25:10 -0500

    1

--- unity-assets_ui/Assets/Scripts/PauseMenu.cs
+++ unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
     public UnityEvent<bool> ToggleMenu;
     public UnityEvent<bool> ToggleMenuInverted;
+
+    private bool isPaused = false;
 
     public void OnEnable()
     {
@@ -38,14 +39,37 @@ public class PauseMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    public void TogglePause()
+    {
+        if (isPaused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+
     public void Pause()
     {
-        gameObject.SetActive(true);
+        Time.timeScale = 0f; // Pause the game by setting the time scale to 0.
+        isPaused = true;
+        gameObject.SetActive(true); // Show the PauseMenu canvas.
     }
 
     public void Resume()
     {
-        gameObject.SetActive(false);
+        Time.timeScale = 1f; // Resume the game by setting the time scale back to 1.
+        isPaused = false;
+        gameObject.SetActive(false); // Hide the PauseMenu canvas.
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause(); // Toggle pause when the Escape key is pressed.
+        }
+    }
 }
